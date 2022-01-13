c6c54ad75a608ce43cd897624121337492261ca7
Jap <jeet63240@gmail.com>
2022-01-13 20:09:18 +0530

    Add one more program of C#.

---
+++ Programming in C# (CS-20)/C#/numPyramidInSequence.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+
+    public static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("" + (j));
+
+            }
+            Console.WriteLine("");
+
+        }
+    }
+}

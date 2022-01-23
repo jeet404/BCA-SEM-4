6260c8a8c7c6694c1aa02ca8fce4a6d0c4d430fd
Jap <jeet63240@gmail.com>
2022-01-23 19:22:47 +0530

    Start making Marksheet with user defined method.

---
+++ Programming in C# (CS-20)/C#/marksheetWithUDMethods.cs
@@ -0,0 +1,35 @@
+using System;
+
+class stud
+{
+    public string name;
+    public int rno;
+    public string[] sub = new string[6] {
+            "Chemistry", "Physics", "Mathes",
+            "GK", "English", "Computer"
+        };
+    public int[] marks = new int[6];
+    public int totalMarks = 600;
+    public int obtainMarks = 0;
+    public float per;
+
+    public static void Main()
+    {
+
+    }
+
+    public static void get_data()
+    {
+
+    }
+
+    public static void cal_data()
+    {
+
+    }
+
+    public static void display()
+    {
+
+    }
+}

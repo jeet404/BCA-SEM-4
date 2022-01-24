50170b665389422597ce512818452c000ab8fec3
Jap <jeet63240@gmail.com>
2022-01-24 19:19:14 +0530

    Add Entry Control loops Program in Java.

---
+++ Programming in JAVA (CS-19)/Java/forLoopDemo.java
@@ -0,0 +1,10 @@
+public class forLoopDemo {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i <= 5; i++) {
+            System.out.println("Value for i = " + i);
+        }
+        System.out.println("Proccess Over");
+    }
+}
---
+++ Programming in JAVA (CS-19)/Java/whileLoopDemo.java
@@ -0,0 +1,12 @@
+public class whileLoopDemo {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Value for i = " + i);
+            i++;
+        }
+        System.out.println("Proccess Over");
+    }
+}

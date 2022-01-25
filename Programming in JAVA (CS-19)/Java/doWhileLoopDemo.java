66f06f335c0273349635be093609888cf0a2ba9b
Jap <jeet63240@gmail.com>
2022-01-25 08:26:34 +0530

    Add Exit Control Loop Program in JAVA.

---
+++ Programming in JAVA (CS-19)/Java/doWhileLoopDemo.java
@@ -0,0 +1,12 @@
+public class doWhileLoopDemo {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        do {
+            System.out.println("Value for i = " + i);
+            i++;
+        } while (i <= 5);
+        System.out.println("Proccess Over");
+    }
+}

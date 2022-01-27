8c93c94e17a1be00fcd72e19c8821e3b6828bc44
jeet404 <jeet63240@gmail.com>
2022-01-27 14:44:02 +0530

    Fix code.

--- Programming in C# (CS-20)/C#/marksheetWithUDMethods.cs
+++ Programming in C# (CS-20)/C#/marksheetWithUDMethods.cs
@@ -2,20 +2,19 @@ using System;
 
 class stud
 {
-    string name;
-    int rno;
-    string[] sub = new string[6] {
+    static string name;
+    static int rno;
+    static string[] sub = new string[6] {
             "Chemistry", "Physics", "Mathes",
             "GK", "English", "Computer"
         };
-    int[] marks = new int[6];
-    int totalMarks = 600;
-    int obtainMarks = 0;
-    float per;
+    static int[] marks = new int[6];
+    static int totalMarks = 600;
+    static int obtainMarks = 0;
+    static float per;
 
     public static void Main()
     {
-        stud s = new stud();
         get_data();
         cal_data();
         display();
@@ -23,64 +22,61 @@ class stud
 
     public static void get_data()
     {
-        stud put = new stud();
         Console.Write("Enter Roll No : ");
-        put.rno = Int32.Parse(Console.ReadLine());
+        rno = Int32.Parse(Console.ReadLine());
         Console.Write("Enter Name : ");
-        put.name = Console.ReadLine();
-        for (int i = 0; i < put.marks.Length; i++)
+        name = Console.ReadLine();
+        for (int i = 0; i < marks.Length; i++)
         {
-            Console.Write("Enter Marks of {0} = ", put.sub[i]);
-            put.marks[i] = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter Marks of {0} = ", sub[i]);
+            marks[i] = Int32.Parse(Console.ReadLine());
         }
         Console.WriteLine();
     }
 
     public static void cal_data()
     {
-        stud cal = new stud();
-        for (int i = 0; i < cal.marks.Length; i++)
+        for (int i = 0; i < marks.Length; i++)
         {
-            cal.obtainMarks = cal.obtainMarks + cal.marks[i];
+            obtainMarks = obtainMarks + marks[i];
         }
-        cal.per = cal.obtainMarks / cal.totalMarks * 100;
+        per = obtainMarks / totalMarks * 100;
     }
 
     public static void display()
     {
-        stud s = new stud();
         Console.WriteLine("******************************************************");
         Console.WriteLine("*                    Final Marksheet                 *");
         Console.WriteLine("******************************************************");
-        Console.WriteLine("*  Roll No : {0}                                     *", s.rno);
-        Console.WriteLine("*  Name : {0}                                        *", s.name);
+        Console.WriteLine("*  Roll No : {0,-5}                                     *", rno);
+        Console.WriteLine("*  Name : {0,-19}                                        *", name);
         Console.WriteLine("******************************************************");
-        for (int i = 0; i < s.sub.Length; i++)
+        for (int i = 0; i < sub.Length; i++)
         {
-            Console.Write("{0, -5}", i + 1);
-            Console.Write("{0, -19} {1}", s.sub[i], s.marks[i]);
+            Console.Write("{0}", i + 1);
+            Console.Write("{0} {1}", sub[i], marks[i]);
             Console.WriteLine();
         }
         Console.WriteLine("******************************************************");
-        Console.WriteLine("*  Obtained Marks : {0}                              *", s.obtainMarks);
-        Console.WriteLine("*  Percentage : {0}                                  *", s.per);
-        if (s.per >= 85)
+        Console.WriteLine("*  Obtained Marks : {0}                              *", obtainMarks);
+        Console.WriteLine("*  Percentage : {0}                                  *", per);
+        if (per >= 85)
         {
             Console.WriteLine("*  Remarks : Excellent                           *");
         }
-        else if (s.per < 75)
+        else if (per < 75)
         {
             Console.WriteLine("*  Remarks : Very Good                           *");
         }
-        else if (s.per < 65)
+        else if (per < 65)
         {
             Console.WriteLine("*  Remarks : Good                                *");
         }
-        else if (s.per < 45)
+        else if (per < 45)
         {
             Console.WriteLine("*  Remarks : Need Improvement                    *");
         }
-        else if (s.per > 33)
+        else if (per > 33)
         {
             Console.WriteLine("*  Remarks : Fail, Better Luck Next Tim          *");
         }

daf48a1284fbdd1815969aee1f84f2e0b9ca02b9
jeet404 <jeet63240@gmail.com>
2022-02-04 19:42:50 +0530

    Add Math Class Program in JAVA.

---
+++ Programming in JAVA (CS-19)/Java/methClassDemo.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class methClassDemo {
+    public static void main(String[] args) {
+        int v1 = -1;
+        float v2 = .5f;
+        System.out.println("Initial Value of V1 = " + v1);
+        System.out.println("Initial Value of V2 = " + v2);
+        int absI = Math.abs(v1);
+        float absF = Math.abs(v2);
+        System.out.println("Absolute Value of V1 = " + absI);
+        System.out.println("Absolute Value of V2 = " + absF);
+    }
+}

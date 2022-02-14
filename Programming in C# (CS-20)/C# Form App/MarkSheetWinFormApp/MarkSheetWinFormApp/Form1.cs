4e27c1199fd02de03c85046a23eea1fd621c7bb1
jeet404 <jeet63240@gmail.com>
2022-02-14 13:28:18 +0530

    Add Marksheet Program in C# using Windows Form Application.

---
+++ Programming in C# (CS-20)/C# Form App/MarkSheetWinFormApp/MarkSheetWinFormApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarkSheetWinFormApp
+{
+    public partial class Form1 : Form
+    {
+        int cs, java, os, net;
+        int total = 0;
+        float per = 0;
+        string grade = "";
+        string newline = Environment.NewLine;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cs = Int32.Parse(txt_cs.Text);
+            java = Int32.Parse(txt_java.Text);
+            os = Int32.Parse(txt_os.Text);
+            net = Int32.Parse(txt_net.Text);
+
+            total = cs + java + os + net;
+            per = (total / 4);
+            if (per >= 80)
+            {
+                grade = "A+";
+            }
+            else if (per >= 70)
+            {
+                grade = "A";
+            }
+            else if (per >= 60)
+            {
+                grade = "B";
+            }
+            else if (per >= 35)
+            {
+                grade = "C";
+            }
+            else
+            {
+                grade = "Fail";
+            }
+
+            txt_result.Text = "Roll No : " +txt_rno.Text+newline+"Student Name : "+txt_studname.Text+newline+"-------------------------------"+newline+"C#     "+cs+newline+"JAVA    "+java+newline+"OS    "+os+newline+"Network   "+net+newline+"-------------------------------"+newline+"Obtained Marks : "+total+newline+"Percentage : "+per+newline+"Grade : "+grade+newline+"-------------------------------";
+        }
+    }
+}

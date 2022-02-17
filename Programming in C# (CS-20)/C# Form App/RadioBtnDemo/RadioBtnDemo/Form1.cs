9d81600e8c803158562a738e93718a3c3cc94f8d
jeet404 <jeet63240@gmail.com>
2022-02-17 09:17:40 +0530

    Add new program in C# Win Forms App.

---
+++ Programming in C# (CS-20)/C# Form App/RadioBtnDemo/RadioBtnDemo/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioBtnDemo
+{
+    public partial class Form1 : Form
+    {
+        string lbl_data = "You have selected";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rbtn_red_CheckedChanged(object sender, EventArgs e)
+        {
+            rbtn_red.BackColor = Color.Red;
+            rbtn_red.Text = lbl_data + rbtn_red.Text;
+        }
+
+        private void rbtn_yellow_CheckedChanged(object sender, EventArgs e)
+        {
+            rbtn_yellow.BackColor = Color.Yellow;
+            rbtn_yellow.Text = lbl_data + rbtn_yellow.Text;
+        }
+    }
+}

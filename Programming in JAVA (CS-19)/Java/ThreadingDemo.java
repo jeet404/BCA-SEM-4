1f7b238e034a315a973f982f375dd6a3076f99ed
jeet404 <jeet63240@gmail.com>
2022-02-23 15:52:38 +0530

    Create Threading Program.

---
+++ Programming in JAVA (CS-19)/Java/ThreadingDemo.java
@@ -0,0 +1,38 @@
+class ABC implements Runnable {
+    public void run() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        System.out.println(
+                "The State of Thread T1 while it invoked the method join() on thread T2 "
+                        + ThreadingDemo.T1.getState());
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+}
+
+public class ThreadingDemo implements Runnable {
+    public static Thread T1;
+    public static ThreadingDemo obj;
+
+    public static void main(String[] args) {
+        obj = new ThreadingDemo();
+        T1 = new Thread(obj);
+        System.out.println("The State of Thread T1 after spawing it " + T1.getState());
+        T1.start();
+        System.out.println("The State of Thread T1 after invoking method start() on it " + T1.getState());
+        public void run()
+        {
+            ABC myobj = new ABC();
+            Thread T2 = new Thread(myobj);
+            System.out.println("The State of Thread T2 after spawing it "+T2.getStat());
+            T2.start();
+            
+        }
+    }
+}

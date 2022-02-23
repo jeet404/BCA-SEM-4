d1b9fb9d808a182d8f681337aabed6ff2ee7324b
jeet404 <jeet63240@gmail.com>
2022-02-23 18:56:55 +0530

    Complate Code of Threading in JAVA.

--- Programming in JAVA (CS-19)/Java/ThreadingDemo.java
+++ Programming in JAVA (CS-19)/Java/ThreadingDemo.java
@@ -26,13 +26,25 @@ public class ThreadingDemo implements Runnable {
         System.out.println("The State of Thread T1 after spawing it " + T1.getState());
         T1.start();
         System.out.println("The State of Thread T1 after invoking method start() on it " + T1.getState());
-        public void run()
-        {
-            ABC myobj = new ABC();
-            Thread T2 = new Thread(myobj);
-            System.out.println("The State of Thread T2 after spawing it "+T2.getStat());
-            T2.start();
-            
+    }
+
+    public void run() {
+        ABC myobj = new ABC();
+        Thread T2 = new Thread(myobj);
+        System.out.println("The State of Thread T2 after spawing it " + T2.getState());
+        T2.start();
+        System.out.println("The State of Thread T2 after calling the method start() on it " + T2.getState());
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
+        System.out.println("The State of Thread T2 after calling the method start() on it " + T2.getState());
+        try {
+            T2.join();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        System.out.println("The State of Thread T2 when it has complated it's execution " + T2.getState());
     }
 }

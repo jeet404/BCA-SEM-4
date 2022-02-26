cca6567c5b3f3c35270ca951c6c51c717d218910
jeet404 <jeet63240@gmail.com>
2022-02-26 14:17:38 +0530

    Add Pannel Demo.

---
+++ Programming in C# (CS-20)/C# Form App/PanelDemoWinApp/PanelDemoWinApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PanelDemoWinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            RadioButton rdo = new RadioButton();
+            pnl_main.AutoScroll = true;
+            pnl_main.Controls.Add(rdo);        }
+    }
+}

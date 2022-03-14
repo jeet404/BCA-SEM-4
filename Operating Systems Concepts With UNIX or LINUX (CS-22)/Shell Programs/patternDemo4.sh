84cd24aa961bc389be89dd2d66dff7d49a37c8f7
jeet404 <jeet63240@gmail.com>
2022-03-14 21:52:18 +0530

    Add new Materials.

--- Operating Systems Concepts With UNIX or LINUX (CS-22)/Shell Programs/patternDemo4.sh
+++ Operating Systems Concepts With UNIX or LINUX (CS-22)/Shell Programs/patternDemo4.sh
@@ -1,16 +1,15 @@
 n=9
 n=`expr $n/2`
-
 i=`expr $i-$n`
 while [ $i -le $n ]; do
     ii = `expr ${i#-}`
     j=1
     while [ $j -le $ii ]; do
-        echo " "
-        j=`expr $j+1`
+         echo " H"
+        # j = `expr $j + 1`
     done
     j=$ii
-    while [ $j -le $n ]; do
+     while [ $j -le $n ]; do
         if [ $i -lt 0 ]; then
             echo "* "
         elif [ $i -lt 0 ]; then

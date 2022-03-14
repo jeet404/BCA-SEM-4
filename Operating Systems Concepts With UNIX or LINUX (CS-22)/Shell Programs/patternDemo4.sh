cceb35dfd589d0fa3c1c5a36fa8a65806aa3160c
jeet404 <jeet63240@gmail.com>
2022-03-14 11:30:27 +0530

    Add New Program in Shall.

--- Operating Systems Concepts With UNIX or LINUX (CS-22)/Shell Programs/patternDemo4.sh
+++ Operating Systems Concepts With UNIX or LINUX (CS-22)/Shell Programs/patternDemo4.sh
@@ -1,4 +1,25 @@
-echo "Enter Num : "
-read num
-echo
+n=9
+n=`expr $n/2`
 
+i=`expr $i-$n`
+while [ $i -le $n ]; do
+    ii = `expr ${i#-}`
+    j=1
+    while [ $j -le $ii ]; do
+        echo " "
+        j=`expr $j+1`
+    done
+    j=$ii
+    while [ $j -le $n ]; do
+        if [ $i -lt 0 ]; then
+            echo "* "
+        elif [ $i -lt 0 ]; then
+            echo "- "
+        else
+            echo "= "
+            j=`expr $j+1`
+        fi
+    done
+    echo "\n"
+    i=`expr $i+1`
+done

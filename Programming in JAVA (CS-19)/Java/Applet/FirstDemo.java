a06ff6baabcbeeb0fcd6de2e1db14f291aabae3a
jeet404 <jeet63240@gmail.com>
2022-03-16 12:15:06 +0530

    Rename File Name.

---
+++ Programming in JAVA (CS-19)/Java/Applet/FirstDemo.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class FirstDemo extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("WelCome To Applet", 150, 150);
+    }
+}
+/*
+<Applet code="FirstDemo.java" width="300" height="300"></Applet>
+ */

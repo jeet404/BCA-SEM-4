7461ff5f65bb6b6e6699462f992acf414c531d0a
jeet404 <jeet63240@gmail.com>
2022-03-19 14:51:28 +0530

    Create GUI Folder And Add One Program of Frame Initilization in JAVA.

---
+++ Programming in JAVA (CS-19)/Java/GUI/FrameAWTDemo.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class FrameAWTDemo {
+    FrameAWTDemo() {
+        Frame f = new Frame();
+        Button b = new Button();
+        b.setBounds(30, 50, 80, 30);
+        f.add(b);
+        f.setSize(300, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FrameAWTDemo f = new FrameAWTDemo();
+    }
+}

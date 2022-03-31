74a0f62513a82b56eda8b437d6f1d3659370cf70
jeet404 <jeet63240@gmail.com>
2022-03-31 19:58:06 +0530

    Fix Error.

--- Programming in JAVA (CS-19)/Java/IOClassPrograms/dataInputStreamDemo.java
+++ Programming in JAVA (CS-19)/Java/IOClassPrograms/dataInputStreamDemo.java
@@ -1,14 +1,15 @@
 import java.io.*;
 
 public class dataInputStreamDemo {
-    public static void main(String[] args) throws Exception {
-        InputStream input = new FileInputStream("C:\\check\\test.txt");
+    public static void main(String[] args) throws IOException {
+        InputStream input = new FileInputStream("C:\\check\\testout.txt");
         DataInputStream inst = new DataInputStream(input);
         int count = input.available();
         byte[] ary = new byte[count];
+        inst.read(ary);
         for (byte bt : ary) {
             char k = (char) bt;
-            System.out.println(k + "-");
+            System.out.print(k + "-");
         }
     }
 }
